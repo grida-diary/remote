4e4e48643f1035384bb95eae0b7a33ccaaa610ad
wwan13 <wwan13@naver.com>
2024-07-22 23:55:55 +0900

    fix : 파일 이름 수정

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@ sudo docker-compose down -f ./remote/docker-compose.app.yml down
 sudo docker-compose down -f ./remote/docker-compose.infra.yml down
 
 app_images=$(docker-compose -f docker-compose.app.yml config | awk '/image:/ { print $2 }')
-infra_images=$(docker-compose -f docker-compose.infra config | awk '/image:/ { print $2 }')
+infra_images=$(docker-compose -f docker-compose.infra.yml config | awk '/image:/ { print $2 }')
 images=("${app_images[@]}" "${infra_images[@]}")
 for image in "${images[@]}"
 do 
