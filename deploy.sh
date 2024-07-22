52b31622f8f63676fa313f40a4255b747f378b85
wwan13 <wwan13@naver.com>
2024-07-23 00:02:24 +0900

    fix : 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 sudo docker-compose down -f ./remote/docker-compose.app.yml down
 
-images=$(docker-compose -f docker-compose.app.yml config | awk '/image:/ { print $2 }')
+images=$(sudo docker-compose -f ./remote/docker-compose.app.yml config | awk '/image:/ { print $2 }')
 for image in "${images[@]}"
 do 
     echo $image
