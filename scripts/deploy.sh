ebe079ede71bdad03e1b156e1321a623d24fe13b
wwan13 <wwan13@naver.com>
2024-07-23 13:41:36 +0900

    feat : 단계별 echo 출력

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
+echo @@@ stop docker containers
 sudo docker-compose -f ./remote/docker-compose.app.yml down
 
+echo @@@ remove app image
 sudo docker rmi wwan13/grida-core-api:prod
 
-sudo docker-compose up -f ./remote/docker-compose.app.yml up -d
+echo @@@ start docker comtainers
+sudo docker-compose -f ./remote/docker-compose.app.yml up -d
