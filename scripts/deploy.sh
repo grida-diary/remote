f65f6a704989dacfa9b501d3dc4e10f140862ed2
wwan13 <wwan13@naver.com>
2024-11-05 01:04:08 +0900

    client 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,7 @@ sudo docker-compose -f ./remote/docker-compose.yml down
 
 echo @@@ remove app image
 sudo docker rmi wwan13/grida-core-api:prod
+sudo docker rmi wwan13/grida-client:prod
 
 echo @@@ start docker comtainers
 sudo docker-compose -f ./remote/docker-compose.yml up -d
